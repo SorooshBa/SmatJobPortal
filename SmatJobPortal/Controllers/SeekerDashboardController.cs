51168c3169687795b100caafbf7260f79294cdb0
soroosh bagherieh <soroosh1231@outlook.com>
2025-09-03 17:08:05 +0330

    .

--- SmatJobPortal/Controllers/SeekerDashboardController.cs
+++ SmatJobPortal/Controllers/SeekerDashboardController.cs
@@ -1,14 +1,22 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using SmatJobPortal.Data;
+using SmatJobPortal.Models;
 
 namespace SmatJobPortal.Controllers
 {
     [Authorize(Roles = "JobSeeker")]
-    public class SeekerDashboardController : Controller
+    public class SeekerDashboardController(ApplicationDbContext _db,UserManager<ApplicationUser>_user) : Controller
     {
         public IActionResult Index()
         {
-            return View();
+            var model = new SeekerDashboardModel();
+            model.JobList=_db.Jobs.OrderByDescending(x=>x.AddedTime).Take(3).ToList();
+            model.JobApplyList = _db.JobApply.Where(x=>x.UserId==_user.GetUserAsync(User).Result.Id).OrderByDescending(x=>x.AppliedAt).Take(3).ToList();
+            model.Applications = model.JobList.Count;
+            model.ProfileViews = 0;
+            return View(model);
         }
     }
 }
---
+++ SmatJobPortal/Models/SeekerDashboardModel.cs
@@ -0,0 +1,13 @@
+﻿using SmatJobPortal.Data;
+using SmatJobPortal.Data.Migrations;
+
+namespace SmatJobPortal.Models
+{
+    public class SeekerDashboardModel
+    {
+        public List<Job>? JobList { get; set; }
+        public List<JobApply>? JobApplyList { get; set; }
+        public int Applications { get; set; }
+        public int ProfileViews { get; set; }
+    }
+}
