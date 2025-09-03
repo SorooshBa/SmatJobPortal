a3f0acbea43bc8c96f4178529f3a91c016167d17
soroosh bagherieh <soroosh1231@outlook.com>
2025-09-03 09:31:40 +0330

    view job now works

---
+++ SmatJobPortal/Controllers/JobController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SmatJobPortal.Data;
+using System.Threading.Tasks;
+
+namespace SmatJobPortal.Controllers
+{
+    [Authorize]
+    public class JobController(ApplicationDbContext _db, UserManager<ApplicationUser> _user) : Controller
+    {
+        public async Task<IActionResult> Index(int id)
+        {
+            var model = _db.Jobs.Where(x => x.Id == id).FirstOrDefault();
+            if (model != null)
+            {
+                model.EmployerUser = await _user.FindByIdAsync(model.EmployerUserId);
+                return View(model);
+            }
+            return NotFound();
+        }
+    }
+}
